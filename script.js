7e2eeb450c7e8c8b625ce9cb43b160745e8d2650
MaliksDashboard <maliksdashboard@outlook.com>
2025-03-25 15:08:31 +0200

    add v

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const sectionDurations = [
     20, 15, 15, 30, 15, 20, 15, 15, 15, 15, 15, 27, 15, 15, 20, 15, 45, 15, 15,
-    15, 15, 60,
+    15, 15, 60, 324,
   ];
 
   function scrollToNextSection() {
